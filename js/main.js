19e8503f23e4ae8ae4bd02efa103ba1c5c290fc6
Alena59 <asdalena59@gmail.com>
2022-10-02 14:06:20 +0300

    сделала правки

--- js/main.js
+++ js/main.js
@@ -2,19 +2,21 @@ function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   if (min > max || min < 0 || max < 0) {
-    return ('Задан неверный диапазон! Укажите другие числа.');
+    return NaN
   }
 
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-console.log(getRandomInt(7, 9));
+
+getRandomInt(2, 6);
 
 function getRandomArbitrary(min, max, maxDigits = 0) {
   if (min > max || min < 0 || max < 0) {
-    return ('Задан неверный диапазон! Укажите другие числа.');
+    return NaN
   }
 
   const digitsDegree = 10 ** maxDigits;
-  return ~~((Math.random() * (max - min) + min) * digitsDegree) / digitsDegree;
+  return Math.floor((Math.random() * (max - min) + min) * digitsDegree) / digitsDegree;
 }
-console.log(getRandomArbitrary(1, 2, 6));
+
+getRandomArbitrary(2, 6, 7);
