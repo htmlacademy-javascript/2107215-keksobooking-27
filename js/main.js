a0b1ec8109cd5876f2bce5e32b8d4775459d8da9
Alena59 <asdalena59@gmail.com>
2022-10-11 21:26:29 +0300

    сделала правки

--- js/main.js
+++ js/main.js
@@ -1,23 +1,4 @@
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  if (min > max || min < 0 || max < 0) {
-    return NaN;
-  }
-
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function getRandomArbitrary(min, max, maxDigits = 0) {
-  if (min > max || min < 0 || max < 0) {
-    return NaN;
-  }
-
-  const digitsDegree = 10 ** maxDigits;
-  return Math.floor((Math.random() * (max - min) + min) * digitsDegree) / digitsDegree;
-}
-
-const TYPE = [
+const TYPE_OF_HOUSING = [
   'palace',
   'flat',
   'house',
@@ -45,11 +26,30 @@ const FEATURES = [
 const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
 ];
 
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  if (min > max || min < 0 || max < 0) {
+    return NaN;
+  }
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomArbitrary(min, max, maxDigits = 0) {
+  if (min > max || min < 0 || max < 0) {
+    return NaN;
+  }
+
+  const digitsDegree = 10 ** maxDigits;
+  return Math.floor((Math.random() * (max - min) + min) * digitsDegree) / digitsDegree;
+}
+
 function getArray(features) {
-  const maxLength = features.length;
+  const maxLength = features.length - 1;
   const lengthOfArray = getRandomInt(1, maxLength);
   const array = [];
 
@@ -66,16 +66,15 @@ function getArray(features) {
 
 const getRandomArrayElement = (elements) => elements[getRandomInt(0, elements.length - 1)];
 
-const creatAutorData = (index) => ({
-  avatar: `img/avatars/user${index.toString().padStart(2, '0')}.png`,
-});
-
-const createCard = () => ({
+const createCard = (index) => ({
+  author: {
+    avatar: `img/avatars/user${index.toString().padStart(2, '0')}.png`,
+  },
   offer: {
     title: 'Добро пожаловать в наше уютное жилье',
     address: (`${getRandomArbitrary(35.65000, 35.70000, 5)}, ${getRandomArbitrary(139.70000, 139.80000, 5)}`),
     price: getRandomInt(1, 10),
-    type: getRandomArrayElement(TYPE),
+    type: getRandomArrayElement(TYPE_OF_HOUSING),
     rooms: getRandomInt(1, 10),
     guests: getRandomInt(1, 10),
     checkin: getRandomArrayElement(CHECKIN),
@@ -86,14 +85,10 @@ const createCard = () => ({
   },
   location: {
     lat: getRandomArbitrary(35.65000, 35.70000, 5),
-    Lng: getRandomArbitrary(139.70000, 139.80000, 5),
+    lng: getRandomArbitrary(139.70000, 139.80000, 5),
   }
 });
 
-const creatOffer = (index) => ({
-  autor: creatAutorData (index),
-});
+const createOffers = () => Array.from({length: 10}, (_, index) => createCard(index + 1));
 
-const getOffers = Array.from({length: 10}, (_, index) => creatOffer(index + 1));
-
-const similarCards = Array.from({length: 10}, createCard);
+createOffers();
