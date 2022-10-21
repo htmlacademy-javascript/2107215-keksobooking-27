48fd408fb55fa5dffc0067b7fb3d618f5c481734
Alena59 <asdalena59@gmail.com>
2022-10-21 17:48:50 +0300

    доработки 2

--- js/form.js
+++ js/form.js
@@ -1,6 +1,10 @@
 const adForm = document.querySelector('.ad-form');
+const mapFilters = document.querySelector('.map__filters');
+const slider = document.querySelector('.ad-form__slider');
+
 
 function disableElement(classElement) {
+  classElement.classList.add(`${classElement.classList[0]}--disabled`);
   for (const item of classElement.children) {
     item.disabled = true;
   }
@@ -8,18 +12,23 @@ function disableElement(classElement) {
 
 export function deactivePage() {
   disableElement(adForm);
+  disableElement(mapFilters);
+  slider.setAttribute('disabled', true);
 }
 
 deactivePage();
 
 function enableElement(classElement) {
+  classElement.classList.remove(`${classElement.classList[0]}--disabled`);
   for (const item of classElement.children) {
     item.disabled = false;
   }
 }
 
 export function activePage() {
+  disableElement(mapFilters);
   enableElement(adForm);
+  slider.removeAttribute('disabled');
 }
 
 activePage();
